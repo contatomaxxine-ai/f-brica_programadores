f10a89e2484fddb116e678130045c7b8074607ac
Karina Sousa <contatomaxxine@gmail.com>
2026-05-09 11:45:02 -0300

    calculo do imc

---
+++ 03_python_entradas/entrada6.py
@@ -0,0 +1,9 @@
+# autor: Karina sousa
+# Projeto: IMC
+
+peso = float(input('digite o valor do peso: '))
+altura = float(input('digite o valor da altura: '))
+imc = peso / (altura * altura)
+
+# exibir o resultado com variável
+print('o resultado é: ',imc)
