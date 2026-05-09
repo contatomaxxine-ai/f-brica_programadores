84ba130bb4d0db8609fcc4aef6c57e418f0e7900
Karina Sousa <contatomaxxine@gmail.com>
2026-05-09 11:32:06 -0300

    Add files via upload

---
+++ 03_python_entradas/entrada5.py
@@ -0,0 +1,17 @@
+# autor Karina sousa
+# Projeto: calculos matemáticos
+
+# variaveis
+nome = input('Digite seu nome: ')
+valor1 = int(input('digite o primeiro valor: '))
+valor2 = int(input('digite o segundo valor: '))
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print(nome)
+print(f'o resultado da soma é: {soma}')
+print(f'o resultado da multiplicacao é: {multiplicacao}')
+print(f'o resultado da subtracao é: {subtracao}')
+print(f'o resultado da divisao é: {divisao}')
