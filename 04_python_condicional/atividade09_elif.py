ea45f561b2bac338c181afc956eb8af4620f9bf0
Karina Sousa <contatomaxxine@gmail.com>
2026-05-23 08:45:02 -0300

    Add files via upload

---
+++ 04_python_condicional/atividade09_elif.py
@@ -0,0 +1,10 @@
+temperatura = float(input('digite a temperatura em Celsius:'))
+
+if temperatura >=30:
+    print('está quente!')
+elif temperatura >= 20:
+    print('está agradável!')
+elif temperatura >=10:
+    print('está frio!')
+else:
+    print('está muito frio!')
