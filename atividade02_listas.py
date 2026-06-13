90796b8731978fee081c925f7da70b9d3103c267
Karina Sousa <contatomaxxine@gmail.com>
2026-06-13 11:49:11 -0300

    Add files via upload

---
+++ atividade02_listas.py
@@ -0,0 +1,11 @@
+#autor: Karina Sousa
+#projeto: listas em python
+
+nomes = ['Enola', 'Sherlock', 'Anne', 'Gilbert', 'Jane', 'Marilla']
+
+print(nomes[0]) # Enola
+print(nomes[1]) # Sherlock
+print(nomes[2]) # Anne
+print(nomes[3]) # Gilbert
+print(nomes[4]) # Jane
+print(nomes[5]) # Marilla
---
+++ atividade03_listas.py
@@ -0,0 +1,16 @@
+#autor: Karina Sousa
+#projeto: listas em python
+
+nomes = ['Enola', 'Sherlock', 'Anne', 'Gilbert', 'Jane', 'Marilla']
+print(nomes)
+
+nomes[1] = 'Wendy'
+print(nomes)
+
+
+print(nomes[0]) #Enola
+print(nomes[1]) #Sherlock
+print(nomes[2]) #Anne
+print(nomes[3]) #Gilbert
+print(nomes[4]) #Jane
+print(nomes[5]) #Marilla
